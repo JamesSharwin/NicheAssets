4310c31be2f52c99122a0764508e1bcdef850ec9
JamesSharwin <jrsharwin@outlook,com>
2019-05-25 21:22:33 +0100

    Update niche_theme.R

--- niche_theme.R
+++ niche_theme.R
@@ -1,11 +1,3 @@
-
-#install.packages("extraFont")
-
-#install.packages("windowsFonts")
-#font_import()
-#loadfonts(device = "win")
-
-install.packages("tidyverse")
 library(extrafont)
 library(tidyverse)
 
@@ -42,76 +34,4 @@ niche_style <- function(){
     strip.text = element_text(family = "Arial",size = 14,hjust = 0,color="black")
   ) 
 }
-
-
-data("ChickWeight")
-
-
-#Multiple line plot
-x <- ChickWeight %>%
-  group_by(Time,Diet) %>%
-  summarise(Weight = mean(weight))
-
-x$Time <- as.factor(x$Time)
-
-ggplot(x,aes(x=Time,y=Weight,color=Diet,group=Diet)) +
-  geom_line(size=1) + 
-  labs(title = "Total chicken weight over time",
-       subtitle = "Really groundbreaking stuff",
-       x = "Time",
-       y = "Weight") +
-  coord_cartesian(ylim=c(0,300)) +
-  scale_y_continuous(expand=c(0,0)) + 
-  niche_style() 
-
-#Single line chart
-x <- ChickWeight %>%
-  group_by(Time) %>%
-  summarise(Weight = mean(weight))
-
-x$Time <- as.factor(x$Time)
-
-ggplot(x,aes(x=Time,y=Weight,group=1)) +
-  geom_line(size=1,color=cols[1]) + 
-  labs(title = "Total chicken weight over time",
-       subtitle = "Really groundbreaking stuff",
-       x = "Time",
-       y = "Weight") +
-  coord_cartesian(ylim=c(0,300)) +
-  scale_y_continuous(expand=c(0,0)) + 
-  niche_style() 
-
-#Single bar chart
-x <- ChickWeight %>%
-  group_by(Time,Diet) %>%
-  summarise(Weight = mean(weight))
-
-x$Time <- as.factor(x$Time)
-
-ggplot(x,aes(x=Time,y=Weight,fill=Diet)) +
-  geom_col() + 
-  labs(title = "Total chicken weight over time",
-       subtitle = "Really groundbreaking stuff",
-       x = "Time",
-       y = "Weight") +
-  scale_y_continuous(expand=c(0,0)) + 
-  niche_style() 
-
-#Faceted bar chart
-x <- ChickWeight %>%
-  group_by(Time,Diet) %>%
-  summarise(Weight = mean(weight))
-
-x$Time <- as.factor(x$Time)
-
-ggplot(x,aes(x=Time,y=Weight,fill=Diet)) +
-  geom_col() + 
-  facet_grid(Diet~.) +
-  labs(title = "Total chicken weight over time",
-       subtitle = "Really groundbreaking stuff",
-       x = "Time",
-       y = "Weight") +
-  scale_y_continuous(expand=c(0,0)) + 
-  coord_cartesian(ylim=c(0,300)) +
-  niche_style() 
 
