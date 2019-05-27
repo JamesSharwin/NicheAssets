62a8172b602455c6e4c55c31f3ae6a38d45d252c
JamesSharwin <jrsharwin@outlook,com>
2019-05-27 18:17:56 +0100

    Commit

--- ggplot_theme/implementation_demo.R
+++ ggplot_theme/implementation_demo.R
@@ -1,18 +1,19 @@
-
+##Font loading if necessary
 #install.packages("extraFont")
-
 #install.packages("windowsFonts")
 #font_import()
 #loadfonts(device = "win")
 
-#install.packages("tidyverse")
 
+#standard library loads
 library(extrafont)
 library(tidyverse)
 library(devtools)
 library(tidyverse)
 
 source_url("https://raw.githubusercontent.com/nicheconsult/NicheAssets/master/ggplot_theme/niche_theme.R")
+
+
 
 
 data("ChickWeight")
@@ -84,4 +85,20 @@ ggplot(x,aes(x=Time,y=Weight,fill=Diet)) +
   scale_y_continuous(expand=c(0,0)) + 
   coord_cartesian(ylim=c(0,300)) +
   niche_style() 
+
+
+ChickWeight$Time <- as.factor(ChickWeight$Time)
+
+x <- ChickWeight %>% filter(Time %in% c(4,8,12,16,20))
+
+ggplot(x,aes(x=weight,fill=Diet)) + 
+  geom_density(color=NA) + 
+  niche_style() +
+  facet_grid(Diet~Time) +
+  labs(
+    title = "Distribution over time",
+    x = "Weight",
+    y = "Point density"
+  )
+
 
--- ggplot_theme/niche_theme.R
+++ ggplot_theme/niche_theme.R
@@ -7,8 +7,10 @@ ggplot <- function(...) ggplot2::ggplot(...) +
 
 niche_style <- function(){
   theme(
-    plot.title = element_text(family = "Arial",size=24, face="bold", color="#222222"),
-    plot.subtitle = element_text(family = "Arial",size=18,margin = margin(5,0,15,0)),
+    plot.title = element_text(family = "Arial",size=24, face="bold", color="#222222",margin = margin(10,0,10,0)),
+    plot.subtitle = element_text(family = "Arial",size=18,margin = margin(10,0,20,0)),
+    
+    
     plot.background = element_rect(fill = "white"),
     panel.grid.minor = element_blank(),
     panel.grid.major.y = element_line(color="Grey85"),
@@ -17,7 +19,8 @@ niche_style <- function(){
     panel.spacing.y = unit(.75,"lines"),
     
     axis.title = element_text(family = "Arial",size=14,color="#222222",margin=margin(2,2,2,2)),
-    axis.text = element_text(family = "Arial",size=12,color="#222222",margin=margin(2,2,2,2)),
+    axis.text = element_text(family = "Arial",size=12,color="#222222",margin=margin(7,2,2,2)),
+    
     axis.ticks = element_blank(),
     axis.line = element_blank(),
     
